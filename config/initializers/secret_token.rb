8d4fb17ed883119c7a810375f0365fb46333fc5e
Chris Johnson <chris@johnsonch.com>
2011-04-22 14:51:21 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SauceGemApp::Application.config.secret_token = 'a0fa6d29a90f859d77fc44b84752c8494acdfc5bb008f09bb2c4e9a0703217ce30c378360251f3a80d8600377e644dfe1c1135edbf37ccb6e1b26a2d0612e0f8'
